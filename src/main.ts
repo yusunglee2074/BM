72326beb3f4b1e07e2caa140505eb9c47fd4b880
yusunglee2074 <lys0333@gmail.com>
2021-12-31 17:41:32 +0900

    fix(*): revert 064ba67

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('PORT');
-
-  await app.listen(PORT);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
